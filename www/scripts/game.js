0e3887e99ab17315888e0606dcf46944f209b6e4
István Donkó <istvan.donko@gmail.com>
2013-07-25 15:14:04 +0200

    Scoring, Undo + New game buttons
    plus a little bit of design modifications

--- www/scripts/game.js
+++ www/scripts/game.js
@@ -3,10 +3,23 @@
 //var colors = ["white", "red", "yellow", "green", "cyan", "blue", "purple", "black"];
 var colors = ["white", "red", "yellow", "green", "cyan", "blue", "purple"];
 
-var field = new Array();
+var field;
+var prevField;
+var score, prevScore;
+
+var undoButton;
 
 function init()
 {
+	document.getElementById("newImg").addEventListener("click", function(){init();}, false);
+	
+	field = new Array();
+	score = 0; prevScore = 0;
+	
+	undoButton = document.getElementById("undoButton");
+	undoButton.disabled = true;
+	undoButton.addEventListener("click", undo, false);
+	
 	var table = document.getElementById("field");
 	
 	var tableContent = "";
@@ -22,29 +35,35 @@ function init()
 			var color = Math.floor(Math.random() * (localStorage.getItem("colorCount"))) + 1;
 			field[i][j] = color;
 			
-			tableContent += "<td class='" + colors[color] + "' id='"+ i + "_" + j +"' onclick='press(" + i + ", " + j + ")'>";
-			
-			tableContent += "</td>";
+			tableContent += "<td id='"+ i + "_" + j +"' onclick='press(" + i + ", " + j + ")'></td>";
 		}
 		
 		tableContent += "</tr>";
 	}
 	
 	table.innerHTML = tableContent;
+	
+	draw();
 }
 
 function press(i, j)
 {
 	if (field[i][j] == 0) {return;};
 	
-	var oldfield = JSON.parse(JSON.stringify(field));
+	var oldfield = copy(field);
 	var marked = field;
 	var counter = {"count": 0};
 	check(marked, i, j, counter);
 	
 	if(counter.count > 1)
 	{
+		prevField = copy(oldfield);
+		prevScore = score;
+		
 		rearrange();
+		score += (counter.count * (counter.count-1)) * 5;
+		draw();
+		undoButton.disabled = false;
 	}
 	
 	else
@@ -116,7 +135,20 @@ function rearrange()
 			}
 		}
 	}
+}
+
+function undo()
+{
+	field = copy(prevField);
+	score = prevScore;
 	
+	undoButton.disabled = true;
+	
+	draw();
+}
+
+function draw()
+{
 	for(var k = 0; k < field.length; k++)
 	{
 		for(var l = 0; l < field[k].length; l++)
@@ -124,4 +156,11 @@ function rearrange()
 			document.getElementById(k + "_" + l).className = colors[field[k][l]];
 		}
 	}
+	
+	document.getElementById("scoreLabel").innerHTML = score;
+}
+
+function copy(object)
+{
+	return JSON.parse(JSON.stringify(object));
 }
